848442275689dd6354a3ffd8b2874d336ae3d713
Kamil Hassan <kamilhassan201@gmail.com>
2023-11-11 11:32:10 +0800

    chore: Add Smooth Transition after Data loading for Course Chat

--- client/src/components/CourseRecommendation.jsx
+++ client/src/components/CourseRecommendation.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useFetch from '../api/useFetch';
 import RecommendationStudentForm from './RecommendationStudentForm';
 import Spinner from './shared/Spinner';
@@ -6,13 +6,21 @@ import RecommendationChat from './RecommendationChat';
 
 export default function CourseRecommendation() {
   const [submitted, setSubmitted] = useState(false);
-
   const { loading, fetchData } = useFetch();
   const [data, setData] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
   const [studentCharacteristics, setStudentCharacteristics] = useState('');
+
+  useEffect(() => {
+    if (!loading) {
+      setIsVisible(true);
+    }
+  }, [loading]);
+
   const handleFormSubmit = async (studentProfile) => {
     setStudentCharacteristics(studentProfile);
     setSubmitted(true);
+    setIsVisible(false);
     try {
       const result = await fetchData({ student_profile: studentProfile });
       setData(result.data);
@@ -20,9 +28,9 @@ export default function CourseRecommendation() {
       alert(`Sorry, something went wrong! ${error.message}`);
     }
   };
+
   return (
-    <div className="h-full ">
-      {/* <div className="text-2xl">Course Recommendation</div> */}
+    <div className="h-full">
       {!submitted ? (
         <RecommendationStudentForm handleFormSubmit={handleFormSubmit} />
       ) : (
@@ -30,10 +38,16 @@ export default function CourseRecommendation() {
           {loading ? (
             <Spinner label="Analyzing your data, please wait for a moment!" />
           ) : (
-            <RecommendationChat
-              studentCharacteristics={studentCharacteristics}
-              courseInfo={data}
-            />
+            <div
+              className={`h-full transition-opacity duration-1000 ${
+                isVisible ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <RecommendationChat
+                studentCharacteristics={studentCharacteristics}
+                courseInfo={data}
+              />
+            </div>
           )}
         </div>
       )}
