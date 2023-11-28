7dee1182a34040843d9a0e752210e4e6e4b9ccd7
Kamil Hassan <kamilhassan201@gmail.com>
2023-11-28 17:36:24 +0800

    chore: course db name change

--- server/app/config.py
+++ server/app/config.py
@@ -15,7 +15,7 @@ configs = {
     "course_characteristics_dataset_path": "data/course_characteristics.csv",
     "all_courses_path": "data/all_courses.json",
     "kb_path": "data/kb.csv",
-    "course_recommendation_vector_cache_path": "vector_db/recommendation/course_recommendations_embeddings_cache.pkl",
+    "course_recommendation_vector_cache_path": "vector_db/recommendation/course_characteristics_embeddings_cache.pkl",
     "simple_knowledge_base_vector_db_path": "vector_db/knowledge_base/index_data",
     "llm_model": "gpt-3.5-turbo",
     "structured_index_name": 'KnowledgeBase'
